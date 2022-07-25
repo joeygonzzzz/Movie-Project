6f8d86c70871fb27c5cfe8dbb5829b887f790d05
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-07-25 14:31:31 -0500

    movie data

--- index.js
+++ index.js
@@ -47,6 +47,11 @@ const resultsWrapper = document.querySelector('.results');
                 <img src="${imgSrc}" />
                 ${movie.Title}
             `;
+            option.addEventListener('click', () => {
+                dropdown.classList.remove('is-active');
+                input.value = movie.Title;
+                onMovieSelect(movie);
+            });
 
             resultsWrapper.appendChild(option);
         }
@@ -57,6 +62,17 @@ const resultsWrapper = document.querySelector('.results');
         if (!root.contains(event.target)) {
             dropdown.classList.remove('is-active');
         }
-    })
+    });
+
+const onMovieSelect = async (movie) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'd9835cc5',
+            i: movie.imdbID
+        }
+    });
+
+    console.log(response.data);
+};
 
 
